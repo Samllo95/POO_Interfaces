845297edb0e601dc7c541cef3ae0a4c405ce0353
samllo95 <samll@DESKTOP-O513SML>
2020-04-16 19:42:45 -0600

    Interfaces

--- src/Ejercicio01/Ejercicio01.java
+++ src/Ejercicio01/Ejercicio01.java
@@ -13,7 +13,6 @@ public class Ejercicio01 {
     public static void main(String[] args) {
         FiguraGeometrica myFig=new FiguraGeometrica();
         System.out.println("El Area del Rectangulo es: " + myFig.calcuArea(4, 7) + "m");
-        System.out.println("\n");
         System.out.println("El Perimetro del Rectangulo es: " + myFig.calcuPerimetro(4, 7) + "m");
     }
 }
